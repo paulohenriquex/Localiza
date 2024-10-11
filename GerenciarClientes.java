c2bbdd834bf202440e1ed9ba7170ad52c977d43a
paulohenriquex <paulohenriquesouzabr@outlook.com>
2024-10-11 16:06:03 -0300

    modificacoes em gerenciar cliente

--- GerenciarClientes.java
+++ GerenciarClientes.java
@@ -23,8 +23,8 @@ public class GerenciarClientes {
                 cadastrarClientes(sc, clientes);
             } else if (opcao == 2) {
                 mostrarClientes(sc, clientes);
-            }else if(opcao == 3){
-                alterarCliente(sc,clientes);
+            } else if (opcao == 3) {
+                alterarCliente(sc, clientes);
             }
 
         } while (opcao != 0);
@@ -63,46 +63,112 @@ public class GerenciarClientes {
             } catch (Exception e) {
                 System.out.println("Formato da data foi digitado errado use o formato dd-MM-yyyy.");
             }
-        }else if(cpfcnpj.length()==15){
-            //!  TODO FAZER MAIS TARDE
-        }else{
+        } else if (cpfcnpj.length() == 15) {
+
+            System.out.println("Digite o nome do cliente: ");
+            String nome = sc.nextLine();
+
+            System.out.println("Digite o nome da cidade: ");
+            String cidade = sc.nextLine();
+
+            System.out.println("Digite o nome da rua: ");
+            String rua = sc.nextLine();
+
+            System.out.println("Digite o número da casa: ");
+            int numero = sc.nextInt();
+            sc.nextLine();
+
+            PessoaJuridica pessoaJuridica = new PessoaJuridica(cpfcnpj, nome, rua, cidade, numero);
+            clientes.add(pessoaJuridica);
+
+        } else {
             System.out.println("O numero digitado está incorreto, por favor digite novamente.");
             return;
         }
     }
 
     public static void mostrarClientes(Scanner sc, ArrayList<Clientes> clientes) {
-        System.out.println("\n");
-        System.out.println("################### Clientes cadastrados ###################");
+        int opcao;
+        System.out.println("1 - Pessoa Física");
+        System.out.println("2 - Pessoa Jurídica");
+        System.out.println("3 - Fisica/Jurídica");
+        opcao = sc.nextInt();
+        sc.nextLine();
 
-        for (Clientes cli : clientes) {
+        if (opcao == 1) {
 
-            if (cli instanceof PessoaFisica) {
-                PessoaFisica pessoaFisica = (PessoaFisica) cli;
-                System.out.println("");
-                System.out.println("Nome: " + pessoaFisica.getNome());
-                System.out.println("CPF: " + pessoaFisica.getCpf());
-                System.out.println("Rua: " + pessoaFisica.getRua());
-                System.out.println("Cidade: " + pessoaFisica.getCidade());
-                System.out.println("Numero: " + pessoaFisica.getNumero());
-                System.out.println("Data de nascimento: " + pessoaFisica.getDataDeNascimento());
+            System.out.println("\n");
+            System.out.println("################### Clientes cadastrados ###################");
+
+            for (Clientes cli : clientes) {
+
+                if (cli instanceof PessoaFisica) {
+                    PessoaFisica pessoaFisica = (PessoaFisica) cli;
+                    System.out.println("");
+                    System.out.println("Nome: " + pessoaFisica.getNome());
+                    System.out.println("CPF: " + pessoaFisica.getCpf());
+                    System.out.println("Rua: " + pessoaFisica.getRua());
+                    System.out.println("Cidade: " + pessoaFisica.getCidade());
+                    System.out.println("Numero: " + pessoaFisica.getNumero());
+                    System.out.println("Data de nascimento: " + pessoaFisica.getDataDeNascimento());
+                }
             }
+
+            System.out.println("\n#########################################################");
+        } else if (opcao == 2) {
+
+            System.out.println("\n");
+            System.out.println("################### Clientes cadastrados ###################");
+
+            for (Clientes cli : clientes) {
+
+                if (cli instanceof PessoaJuridica) {
+                    PessoaJuridica pessoaJuridica = (PessoaJuridica) cli;
+                    System.out.println("");
+                    System.out.println("Nome: " + pessoaJuridica.getNome());
+                    System.out.println("CNPJ: " + pessoaJuridica.getCnpj());
+                    System.out.println("Rua: " + pessoaJuridica.getRua());
+                    System.out.println("Cidade: " + pessoaJuridica.getCidade());
+                    System.out.println("Numero: " + pessoaJuridica.getNumero());
+                }
+            }
+
+            System.out.println("\n#########################################################");
+
+        } else if (opcao == 3) {
+
+            System.out.println("\n");
+            System.out.println("################### Clientes cadastrados ###################");
+
+            for (Clientes cli : clientes) {
+                PessoaJuridica pessoaJuridica = (PessoaJuridica) cli;
+                System.out.println("");
+                System.out.println("Nome: " + pessoaJuridica.getNome());
+                System.out.println("CNPJ: " + pessoaJuridica.getCnpj());
+                System.out.println("Rua: " + pessoaJuridica.getRua());
+                System.out.println("Cidade: " + pessoaJuridica.getCidade());
+                System.out.println("Numero: " + pessoaJuridica.getNumero());
+
+            }
+
+            System.out.println("\n#########################################################");
+
+        } else {
+            System.out.println("Opção inválida");
         }
-        
-        System.out.println("\n#########################################################");
     }
 
-    public static void alterarCliente(Scanner sc, ArrayList<Clientes>clientes){
-        
+    public static void alterarCliente(Scanner sc, ArrayList<Clientes> clientes) {
+
         System.out.println("Digite o nome do cliente que deseja alterar: ");
         String buscarNome = sc.nextLine();
 
-        for(Clientes cli: clientes){
-            if(cli instanceof PessoaFisica){
-                PessoaFisica pessoaFisica = (PessoaFisica)cli;
-                if(pessoaFisica.getNome().equals(buscarNome)){
+        for (Clientes cli : clientes) {
+            if (cli instanceof PessoaFisica) {
+                PessoaFisica pessoaFisica = (PessoaFisica) cli;
+                if (pessoaFisica.getNome().equals(buscarNome)) {
 
-                    System.out.println("Cliente encontrado com sucesso "+ pessoaFisica.getNome());
+                    System.out.println("Cliente encontrado com sucesso " + pessoaFisica.getNome());
 
                     System.out.println("Digite o novo nome: ");
                     String novoNome = sc.nextLine();
@@ -112,7 +178,7 @@ public class GerenciarClientes {
 
                     System.out.println("Digite o novo nome da cidade: ");
                     String novoNomeCidade = sc.nextLine();
-                    
+
                     System.out.println("Digite o novo nome da rua: ");
                     String rua = sc.nextLine();
 
@@ -124,16 +190,16 @@ public class GerenciarClientes {
                     System.out.println("Digite a nova data de nascimento: ");
                     String dataNas = sc.nextLine();
 
-                    try{
+                    try {
                         System.out.println("Entrou no try");
-                        LocalDate dataDeNascimento = LocalDate.parse(dataNas,formatter);
+                        LocalDate dataDeNascimento = LocalDate.parse(dataNas, formatter);
                         System.out.println("1");
                         LocalDate dataAtual = LocalDate.now();
                         System.out.println("2");
                         int verificarIdade = Period.between(dataDeNascimento, dataAtual).getYears();
                         System.out.println("3");
                         System.out.println("Sua idade é: " + verificarIdade);
-                        if(verificarIdade>=18 && verificarIdade <=115){
+                        if (verificarIdade >= 18 && verificarIdade <= 115) {
                             pessoaFisica.setNome(novoNome);
                             pessoaFisica.setCpf(novoCPF);
                             pessoaFisica.setCidade(novoNomeCidade);
@@ -141,7 +207,7 @@ public class GerenciarClientes {
                             pessoaFisica.setNumero(novoNumero);
                             pessoaFisica.setDataDeNascimento(dataDeNascimento);
                         }
-                    }catch(Exception e){
+                    } catch (Exception e) {
                         System.out.println("Falha ao digitar a data. " + e.getMessage());
                     }
 
